8fb2f6f9f0a6885dec8621c7994c6d6941e95396
Bear <bearkovrus@yandex.ru>
2020-03-05 20:43:42 -0500

    Upload

---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+<v-layout column justify-center align-center>
+  <v-flex xs12 sm8 md6 >
+    <v-row>
+      <v-col lg="4" md="4" sm="12" v-for="cards in cards" :key="cards.id">
+        <v-card class="d-inline-flex" min-width="100%">
+          <v-img max-width="100px" :src="cards.img"></v-img>
+          <div>
+            <v-card-title>cheese</v-card-title>
+            <v-card-title>cheese</v-card-title>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-flex>
+</v-layout>
+</template>
+
+<script>
+
+export default {
+  components: {
+  
+  },
+  data() {
+    return {
+      bear: 'brown',
+      cards: [ 
+      {
+        title: 'wolf',
+        button: 'clicky',
+        img: 'https://i.ibb.co/hCWcwMT/Meowsker.png',
+        id: 1,
+      },
+      {
+        title: 'wolf',
+        id: 2,
+      },
+      {
+        title: 'wolf',
+        id: 3,
+      },
+      {
+        title: 'wolf',
+        id: 4,
+      },
+      {
+        title: 'wolf',
+        id: 5,
+      },
+      {
+        title: 'deer',
+        id: 6,
+      },
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,55 @@
+<template> 
+ <v-container style="height: 100vh; overflow: hidden;">
+  <v-row>
+   <v-col cols="12">
+    <v-row align="center" justify="center" style="height: 100vh;">
+        <v-card class="pa-10" min-width="500px">
+            <v-img class="mb-4" max-width="200px" src="https://i.ibb.co/3kqm30Z/placeh.png"></v-img>
+            <v-form>
+                <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+            filled
+          ></v-text-field>
+            <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            label="Password"
+            required
+            filled
+          >
+          </v-text-field>
+           <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            label="Organization Password"
+            required
+            filled
+          ></v-text-field>
+          <v-btn class="green" to="/">Sign in</v-btn>
+           
+            </v-form>
+        </v-card>
+    </v-row>
+   </v-col>
+  </v-row>   
+ </v-container>
+</template>
+
+<script>
+export default {
+    layout: 'loginlayout'
+}
+</script>
+
+<style>
+.backgroundimage {
+    background: url('https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+}
+html,body{
+margin:0;
+}
+
+</style>
